4ab029e9950530853440615ada6b16e6d956fba6
azadei <azadei@code.edu.az>
2023-07-17 14:51:47 +0400

    1

---
+++ 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LinqExercise7
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+
+
+        var m = from x in nums
+                group x by x into y
+                select y;
+       
+
+        foreach (var arrEle in m)
+        {
+            Console.WriteLine(arrEle.Key + "\t" + arrEle.Sum() + "\t\t\t" + arrEle.Count());
+        }
+        
+    }
+}
